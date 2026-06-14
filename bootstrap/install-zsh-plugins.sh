b66530640671955c52dea37f442568514db4373e
RavnSson <c.godtorres@gmail.com>
2026-06-13 22:02:31 -0400

    fix script for zsh plugins

--- bootstrap/install-zsh-plugins.sh
+++ bootstrap/install-zsh-plugins.sh
@@ -6,7 +6,7 @@ PLUGIN_DIR="$HOME/.config/zsh/plugins"
 
 mkdir -p "$PLUGIN_DIR"
 
-while IFS= read =r repo || [[ -n "$repo"]]
+while IFS= read -r repo || [[ -n "$repo" ]]
 do
 [[ -z "$repo" ]] && continue
 [[ "$repo" =~ ^# ]] && continue
@@ -15,9 +15,9 @@ plugin_name=$(basename "$repo" .git)
 
 if [[ ! -d "$PLUGIN_DIR/$plugin_name" ]]; then
 	echo "Installing $plugin_name..."
-	git clone "$repo#" "$PLUGIN_DIR/$plugin_name"
+	git clone "$repo" "$PLUGIN_DIR/$plugin_name"
 else
 	echo "$plugin_name already installed."
 fi
 
-donde < packages/08-zsh-plugins.txt
+done < packages/08-zsh-plugins.txt
